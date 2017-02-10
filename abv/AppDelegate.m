c18cbafc246cb12e448f68b402fb8115ce9f588d
jonhowell <j.a.howell@mmu.ac.uk>
2017-02-10 13:42:12 +0000

    First beta

--- abv/AppDelegate.m
+++ abv/AppDelegate.m
@@ -7,45 +7,132 @@
 //
 
 #import "AppDelegate.h"
+@implementation AppDelegate{
+    BOOL okGoNow;
+}
 
-@interface AppDelegate ()
-
-@end
-
-@implementation AppDelegate
-
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    
+    okGoNow = NO;
+   
+    UIAlertController * alertView =   [UIAlertController
+                                       alertControllerWithTitle:@"ABV App"
+                                       message:@"Read The Instructions First..."
+                                       preferredStyle:UIAlertControllerStyleAlert];
+    
+    alertView.view.tintColor          = [UIColor blueColor];//Agree button colour
+    
+    NSMutableAttributedString *textMessage = [[NSMutableAttributedString alloc] initWithString:@"To see details on how to use this\nApplication and adjust its settings,\nplease read the notes under the\n'(i)Information' Tab-Bar choice.\n\n* Safety Note *\nTake regular breaks and avoid\n strain. If you develop discomfort\n using this App,you must stop\n using it and seek advice.\n\nThis Application is NOT\n for clinical use.\n\nv1.0.1, Copyright © 9.Feb.2017"];
+    
+    long lens = [textMessage length];
+    [textMessage addAttribute:NSFontAttributeName
+     //value:[UIFont systemFontOfSize:10.0] //specific system font
+                        value:[UIFont fontWithName:@"Courier" size:(10.5)] //any other font in the system
+     //range:NSMakeRange(24, 11)];//splits string to when font size starts
+                        range:NSMakeRange(0, lens)];//splits string to when font size starts and ends in characters starting at zero
+    [alertView setValue:textMessage forKey:@"attributedMessage"]; //@"attributedTitle"]; //the text field you want font size and style to effect
+    
+    //the line '* safety note *' bigger font
+    [textMessage addAttribute:NSFontAttributeName
+                        value:[UIFont systemFontOfSize:14.0] //specific system font
+     
+     //range:NSMakeRange(24, 11)];//splits string to when font size starts
+                        range:NSMakeRange(138, 15)];//splits string to when font size starts and ends in characters starting at zero
+    [alertView setValue:textMessage forKey:@"attributedMessage"]; //@"attributedTitle"]; //the text field you want font size and style to effect
+    
+    //the line 'v1.0.1, Copyright © 8.Feb 2017' bigger font
+    [textMessage addAttribute:NSFontAttributeName
+                        value:[UIFont systemFontOfSize:14.0] //specific system font
+     
+     //range:NSMakeRange(24, 11)];//splits string to when font size starts
+                        range:NSMakeRange(lens-30, 30)];//splits string to when font size starts and ends in characters starting at zero
+    [alertView setValue:textMessage forKey:@"attributedMessage"]; //@"attributedTitle"]; //the text field you want font size and style to effect
+    
+    
+    //[[UIView appearanceWhenContainedIn:UIAlertController.class, nil] setBackgroundColor:[UIColor yellowColor]];
+    
+    //[[UIView appearanceWhenContainedIn:UIAlertController.class, nil] setTintColor:[UIColor yellowColor]];
+    
+    alertView.view.layer.cornerRadius = 90.0; //sets the fade in of the background colour
+    alertView.view.backgroundColor    = [UIColor yellowColor]; //centre background colour
+    
+    UIAlertAction* ok = [UIAlertAction
+                         actionWithTitle:@"Agree - RUN App"
+                         //style:UIAlertActionStyleDefault // could be 'destructive' or 'cancel'
+                         style:UIAlertActionStyleDefault
+                         handler:^(UIAlertAction * action)
+                         {
+                         [alertView dismissViewControllerAnimated:YES completion:nil];
+                         //NSLog(@"ok");
+                         //continue to App run
+                         okGoNow = YES;
+                         }];
+    UIAlertAction* cancel = [UIAlertAction
+                             actionWithTitle:@"Do Not Agree - EXIT App"
+                             style:UIAlertActionStyleDestructive
+                             handler:^(UIAlertAction * action)
+                             {
+                             [alertView dismissViewControllerAnimated:YES completion:nil];
+                             //NSLog(@"canceled the App");
+                             okGoNow=NO;
+                             //stop App if cancelled
+                             
+                             if (okGoNow == NO) {
+                                 //NSLog(@"Cancelled");
+                                 //the app has been cancelled by the alert cancel in the accept message in AppDelegate
+                                 //STOP//
+                                 UIApplication *app = [UIApplication sharedApplication];
+                                 [app performSelector:@selector(suspend)];
+                                 
+                                 //wait 2 seconds while app is going background
+                                 [NSThread sleepForTimeInterval:1.0];
+                                 
+                                 //exit app when app is in background
+                                 exit(0);
+                                 //stop
+                             } else {
+                                 //NSLog(@"Running App.");
+                             }
+                             }];
+    
+    [alertView addAction:ok];
+    [alertView addAction:cancel];
+    [self.window addSubview:self.inputViewController.view];
+    [self.window makeKeyAndVisible];
+    
+    [self.window.rootViewController presentViewController:alertView animated:TRUE completion:nil];
+    
     return YES;
 }
 
 
-- (void)applicationWillResignActive:(UIApplication *)application {
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
-
-- (void)applicationWillTerminate:(UIApplication *)application {
+- (void)applicationWillTerminate:(UIApplication *)application
+{
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
-
 
 @end
--- abv/ViewController.m
+++ abv/ViewController.m
@@ -12,18 +12,304 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    BOOL keyboardAnim;
+    float keyboardAnimSpeed;
+    float keyboardAnimDelay;
+}
+
+@synthesize abvTF,
+alcTF,
+alcWtLBL,
+alcVolLBL,
+portionsLBL,
+mixTF,
+mixWtLBL,
+mixVolLBL,
+unitsTF,
+bottleSzTF,
+totWtLBL,
+totVolLBL,
+calculate;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    keyboardAnim = YES;
+    keyboardAnimSpeed = 1;
+    keyboardAnimDelay = 1;
+    
+    alcTF.delegate      = self;
+    mixTF.delegate      = self;
+    unitsTF.delegate    = self;
+    bottleSzTF.delegate = self;
+    
+    [self calculateButtonPressed:nil];
 }
 
+-(void)viewDidAppear:(BOOL)animated{
+    [self calculateButtonPressed:nil];
+}
+
+-(IBAction)calculateButtonPressed:(id)sender{
+    [self doCalculations];
+}
+
+
+
+-(void) doCalculations{
+//Calculations every time the values change
+    
+    //Total volume of drink in glass
+    float totalVol = [unitsTF.text floatValue]*1000 / [abvTF.text floatValue];
+    totVolLBL.text = [NSString stringWithFormat:@"%.1fml", totalVol];//ok
+    
+    //Vol of water in glass
+    float mixVol   = totalVol * (100 - [abvTF.text floatValue]) / 100;
+    mixVolLBL.text = [NSString stringWithFormat:@"%.1fml", mixVol];//ok
+    
+    //Vol of alc in glass
+    float alcVol   = totalVol * [abvTF.text floatValue] / 100;
+    alcVolLBL.text = [NSString stringWithFormat:@"%.1fml", alcVol];//ok
+    
+    //wt of water in glass
+    float mixWt   = totalVol * (100 - [abvTF.text floatValue]) / 100 * [mixTF.text floatValue];
+    mixWtLBL.text = [NSString stringWithFormat:@"%.1fg", mixWt];//
+    
+    //wt of alc in glass
+    float alcWt   = totalVol * [abvTF.text floatValue] / 100 * [alcTF.text floatValue];
+    alcWtLBL.text = [NSString stringWithFormat:@"%.1fg", alcWt];//ok
+    
+    //Total wt of drink in glass
+    float totalWt = alcWt + mixWt;
+    totWtLBL.text = [NSString stringWithFormat:@"%.1fg", totalWt];//ok
+    
+    //portions in bottle
+    float portions   = [bottleSzTF.text floatValue] / totalVol;
+    portionsLBL.text = [NSString stringWithFormat:@"%.1f", portions];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    //used to clear keyboard if screen touched
+    [self.view endEditing:YES];
+    [super touchesBegan:touches withEvent:event];
+}
 
+-(void)textFieldDidBeginEditing:(UITextField *)textField{
+    //***** change all to suit inputs *****
+    //the number refers to the scrolling of the text input field to avoid the keyboard when it appears, then it is moved back afterwards to the 0 origin
+    
+    //no effrect as this App is being ported to iPhone only, even though displaying on an iPad, compromise value
+#define IDIOM    UI_USER_INTERFACE_IDIOM()
+#define IPAD     UIUserInterfaceIdiomPad
+    int yy;
+    if ( IDIOM == IPAD ) {
+        /* do something specifically for iPad. */
+        yy=200;
+    } else {
+        /* do something specifically for iPhone or iPod touch. */
+        yy=200;
+    }
+    
+    // change the color of the text box when you touch it
+    if(textField==self->alcTF){
+        alcTF.backgroundColor = [UIColor greenColor];
+        textField.frame = CGRectMake(textField.frame.origin.x, (textField.frame.origin.y), textField.frame.size.width, textField.frame.size.height);
+        int oft=textField.frame.origin.y-yy;
+        [self keyBoardAppeared:oft];
+    }
+    if(textField==self->mixTF){
+        mixTF.backgroundColor = [UIColor greenColor];
+        textField.frame = CGRectMake(textField.frame.origin.x, (textField.frame.origin.y), textField.frame.size.width, textField.frame.size.height);
+        int oft=textField.frame.origin.y-yy;
+        [self keyBoardAppeared:oft];
+    }
+    if(textField==self->abvTF){
+        abvTF.backgroundColor = [UIColor greenColor];
+        textField.frame = CGRectMake(textField.frame.origin.x, (textField.frame.origin.y), textField.frame.size.width, textField.frame.size.height);
+        int oft=textField.frame.origin.y-yy;
+        [self keyBoardAppeared:oft];
+    }
+    if(textField==self->unitsTF){
+        unitsTF.backgroundColor = [UIColor greenColor];
+        textField.frame = CGRectMake(textField.frame.origin.x, (textField.frame.origin.y), textField.frame.size.width, textField.frame.size.height);
+        int oft=textField.frame.origin.y-yy;
+        [self keyBoardAppeared:oft];
+    }
+    if(textField==self->bottleSzTF){
+        bottleSzTF.backgroundColor = [UIColor greenColor];
+        textField.frame = CGRectMake(textField.frame.origin.x, (textField.frame.origin.y), textField.frame.size.width, textField.frame.size.height);
+        int oft=textField.frame.origin.y-yy;
+        [self keyBoardAppeared:oft];
+    }
+}
+
+-(void)textFieldDidEndEditing:(UITextField *) textField {
+    
+    //move the screen back to the original place
+    [self keyBoardDisappeared:0];
+   
+    //colour change if out of range and insert range max/min value
+    //set backgrounds to yellow/red if had to correct
+
+    if ([alcTF.text floatValue] <= 0.5) {
+        alcTF.textColor = [UIColor redColor];
+        alcTF.text = @"0.5";
+        alcTF.backgroundColor = [UIColor yellowColor];
+    }else{
+        alcTF.textColor = [UIColor blackColor];
+        alcTF.backgroundColor = [UIColor whiteColor];
+    }
+    
+    if ([alcTF.text floatValue] > 1.0) {
+        alcTF.textColor = [UIColor redColor];
+        alcTF.text = @"1.0";
+        alcTF.backgroundColor = [UIColor yellowColor];
+    }else{
+        alcTF.textColor = [UIColor blackColor];
+        alcTF.backgroundColor = [UIColor whiteColor];
+    }
+    
+    if ([mixTF.text floatValue] <= 0.5) {
+        mixTF.textColor = [UIColor redColor];
+        mixTF.text = @"0.5";
+        mixTF.backgroundColor = [UIColor yellowColor];
+    }else{
+        mixTF.textColor = [UIColor blackColor];
+        mixTF.backgroundColor = [UIColor whiteColor];
+    }
+    
+    if ([mixTF.text floatValue] > 4.0) {
+        mixTF.textColor = [UIColor redColor];
+        mixTF.text = @"4.0";
+        mixTF.backgroundColor = [UIColor yellowColor];
+    }else{
+        mixTF.textColor = [UIColor blackColor];
+        mixTF.backgroundColor = [UIColor whiteColor];
+    }
+    
+    if ([unitsTF.text floatValue] <= 0.0) {
+        unitsTF.textColor = [UIColor redColor];
+        unitsTF.text = @"0.0";
+        unitsTF.backgroundColor = [UIColor yellowColor];
+    }else{
+        unitsTF.textColor = [UIColor blackColor];
+        unitsTF.backgroundColor = [UIColor whiteColor];
+    }
+    
+    if ([unitsTF.text floatValue] > 50.0) {
+        unitsTF.textColor = [UIColor redColor];
+        unitsTF.text = @"50.0";
+        unitsTF.backgroundColor = [UIColor yellowColor];
+    }else{
+        unitsTF.textColor = [UIColor blackColor];
+        unitsTF.backgroundColor = [UIColor whiteColor];
+    }
+    
+    if ([bottleSzTF.text floatValue] <= 0.1) {
+        bottleSzTF.textColor = [UIColor redColor];
+        bottleSzTF.text = @"0.1";
+        bottleSzTF.backgroundColor = [UIColor yellowColor];
+    }else{
+        bottleSzTF.textColor = [UIColor blackColor];
+        bottleSzTF.backgroundColor = [UIColor whiteColor];
+    }
+    
+    if ([bottleSzTF.text floatValue] > 100000.0) {
+        bottleSzTF.textColor = [UIColor redColor];
+        bottleSzTF.text = @"100000.0";
+        bottleSzTF.backgroundColor = [UIColor yellowColor];
+    }else{
+        bottleSzTF.textColor = [UIColor blackColor];
+        bottleSzTF.backgroundColor = [UIColor whiteColor];
+    }
+    
+    if ([abvTF.text floatValue] <= 0.1) {
+        abvTF.textColor = [UIColor redColor];
+        abvTF.text = @"0.1";
+        abvTF.backgroundColor = [UIColor yellowColor];
+    }else{
+        abvTF.textColor = [UIColor blackColor];
+        abvTF.backgroundColor = [UIColor whiteColor];
+    }
+    
+    if ([abvTF.text floatValue] > 100000.0) {
+        abvTF.textColor = [UIColor redColor];
+        abvTF.text = @"100000.0";
+        abvTF.backgroundColor = [UIColor yellowColor];
+    }else{
+        abvTF.textColor = [UIColor blackColor];
+        abvTF.backgroundColor = [UIColor whiteColor];
+    }
+    
+    //update calcs
+    [self calculateButtonPressed:nil];
+}
+
+-(void) keyBoardAppeared :(int)oft
+{
+    //move screen up or down as needed to avoid text field entry
+    CGRect frame = self.view.frame;
+    
+    //move frame without anim if toggle in settings indicates yes
+    if (keyboardAnim == YES){
+        
+        //oft= the y of the text field?  make some code to find it
+        [UIView animateWithDuration:keyboardAnimSpeed
+                              delay:keyboardAnimDelay
+                            options: UIViewAnimationOptionCurveEaseOut
+                         animations:^{
+                             self.view.frame = CGRectMake(frame.origin.x, -oft, frame.size.width, frame.size.height);
+                         }
+                         completion:^(BOOL finished){
+                         }];
+    }else{
+        //just move it
+        self.view.frame = CGRectMake(frame.origin.x, -oft, frame.size.width, frame.size.height);}
+}
+
+-(void) keyBoardDisappeared :(int)oft
+{
+    //move the screen back to original position
+    CGRect frame = self.view.frame;
+    //oft= the y of the text field?  make some code to find it
+    if (keyboardAnim == YES){
+        [UIView animateWithDuration:keyboardAnimSpeed
+                              delay:keyboardAnimDelay
+                            options: UIViewAnimationOptionCurveEaseOut
+                         animations:^{
+                             self.view.frame = CGRectMake(frame.origin.x, oft, frame.size.width, frame.size.height);
+                         }
+                         completion:^(BOOL finished){
+                         }];
+    }else{
+        //just move it
+        self.view.frame = CGRectMake(frame.origin.x, -oft, frame.size.width, frame.size.height);}
+    
+    //update calcs
+    
+    [self calculateButtonPressed:nil];
+}
+
+
+- (IBAction)alcChanged:(id)sender {
+    [self doCalculations];
+}
+- (IBAction)mixChanged:(id)sender {
+    [self doCalculations];
+}
+- (IBAction)abvChanged:(id)sender {
+    [self doCalculations];
+}
+- (IBAction)unitsChanged:(id)sender {
+    [self doCalculations];
+}
+- (IBAction)bottleChanged:(id)sender {
+    [self doCalculations];
+}
 @end
